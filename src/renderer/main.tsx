01d55e3d49af1c59ad930804ee08d4feb7e76bb6
JW-Control <jw.control.peru@gmail.com>
2026-07-09 15:37:54 -0500

    feat: enable simple tests runtime

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.js";
 import "./styles.css";
 import "./simple-mode-overrides.css";
+import "./simple-tests-runtime";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
