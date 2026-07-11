9e7603aeaad938e78c1c5e5acfe32b2af922c349
JW-Control <jw.control.peru@gmail.com>
2026-07-11 12:50:50 -0500

    chore(simple): disable strict mode during diagnostics stabilization

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,11 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.js";
 import "./styles.css";
 import "./simple-mode-overrides.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
